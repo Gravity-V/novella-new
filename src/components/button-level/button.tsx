34cb93d1d8f9795122b0fdef857d845d1e062893
xp34 <xp34gvova@gmail.com>
2022-04-05 15:27:43 +0700

    ProgresButtons

--- src/components/button-level/button.tsx
+++ src/components/button-level/button.tsx
@@ -1,8 +1,7 @@
-import { Button, ButtonGroup } from "@mui/material"
-import { buttonStyles } from "./button.style"
-import { Answer } from "../Level"
+import { Button, ButtonGroup } from "@mui/material";
+import { buttonStyles } from "./button.style";
 import React from 'react';
-
+import { Answer } from "../AnswerType";
 
 interface ButtonLevelProps {
   totalLevel: number
@@ -11,14 +10,24 @@ interface ButtonLevelProps {
 }
 
 export function ButtonLevel(props: ButtonLevelProps) {
-  return <ButtonGroup variant="contained" >
+  console.log()
+  const range = (start: number, end: number) => {
+    const length = end - start;
+    return Array.from({ length }, (_, i) => start + i);
+  }
+  return <ButtonGroup variant="contained"  >
     {
-      () => {
+      range(0, props.totalLevel).map(
+        (i) => {
+          const disable = i > props.answers.length ? true : false;
 
-        for (let i = 0; i < props.totalLevel; i++) {
-          <Button>{i}</Button>
+          const color = props.answers[i] ? (props.answers[i].isCorrect ? 'success' : 'error') : 'primary';
+
+
+          return <Button onClick={() => { props.callbackShowLevel(i) }} key={i} color={color} disabled={disable}>{i + 1}</Button>
         }
-      }
+      )
     }
-  </ButtonGroup>
+
+  </ButtonGroup >
 }
