72ee9f3878ddd15bdb5ff2353f6a8a82f6b1815c
xp34 <xp34gvova@gmail.com>
2022-04-05 17:03:45 +0700

    NewLevel AddComent

--- src/components/Questions/Standart.tsx
+++ src/components/Questions/Standart.tsx
@@ -1,25 +1,50 @@
-import { Button } from "@mui/material"
+import { Button, Typography } from "@mui/material"
 import { QuestionStandart } from "../../novella/novellaInterrface"
-import React from 'react';
+import React, { useState } from 'react';
 import { AnswerStandart } from "../AnswerType";
 
 interface StandartProps {
     question: QuestionStandart
-    callbackFinish: (answer: AnswerStandart) => void
+    callbackFinish?: (answer: AnswerStandart) => void
+    answer?: AnswerStandart
+    show?: boolean
+
 }
 
 export function Standart(props: StandartProps) {
+    const [comment, setComment] = useState<string>('')
+    const [answer, setAnswer] = useState<AnswerStandart>()
+    const [lock, setLock] = useState<boolean>(false)
     return <>
         <p>{props.question.text}</p>
         <img src={props.question.background} />
         {props.question.answers.map(
             (answer, i) => <Button
+                variant="outlined"
+                disabled={
+                    props.show === undefined || props.show === false ? lock : props.show
+                }
+                color={props.answer ? (props.answer.userAnswer == answer.text ? 'secondary' : 'primary') : 'primary'}
                 key={i}
                 onClick={() => {
-                    props.callbackFinish({ isCorrect: answer.isCorrect, type: "standart", userAnswer: answer.text })
+                    if (answer.comment) {
+                        setLock(true)
+                        setAnswer({ isCorrect: answer.isCorrect, type: "standart", userAnswer: answer.text })
+                        setComment(answer.comment)
+                    } else {
+                        props.callbackFinish && props.callbackFinish({ isCorrect: answer.isCorrect, type: "standart", userAnswer: answer.text })
+                    }
                 }
                 }
             >{answer.text}</Button>
         )}
+        {answer && <Typography>{comment}</Typography>}
+        {answer && <Button variant="contained"
+            onClick={() => {
+                props.callbackFinish && props.callbackFinish(answer)
+            }}
+        >
+            Далее
+        </Button>}
     </>
 }
