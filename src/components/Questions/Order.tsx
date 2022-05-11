78ee3443fd300b938753e0eb6e313b808e2b3f07
Gravity <semtimsecondadd@gmail.com>
2022-05-11 18:16:27 +0700

    NewOrder_Dnd

--- src/components/Questions/Order.tsx
+++ src/components/Questions/Order.tsx
@@ -1,8 +1,9 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Button, FormControl, Icon, InputLabel, List, ListItem, ListItemIcon, ListItemText, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { QuestionOrder } from '../../novella/novellaInterrface';
 import { AnswerOrder } from '../AnswerType';
 import { DragDropContext, Droppable, Draggable, DropResult, DraggingStyle, NotDraggingStyle } from "react-beautiful-dnd";
+import { DragHandle } from '@mui/icons-material'
 
 interface OrderProps {
     question: QuestionOrder
@@ -11,58 +12,46 @@ interface OrderProps {
     show?: boolean
 }
 
-// export function Order(props: OrderProps) {
-//     // const question: number[] | null[] = new Array(props.question.answers.length).fill(null) // начальная строчка
-//     const [question, setQuestion] = useState<number[]>(new Array(props.question.answers.length).fill(-1))
-//     const handleChange = (value: number, i: number) => {
-//         const newQuestion = [...question]
-//         newQuestion[i] = value
-//         setQuestion(newQuestion)
-//     }
-//     return <>
-//         <div className='OrderBlock'>
-//             <p className='text'>{props.question.text}</p>
-//             {<img src={props.question.background} />}
-//             {
-//                 props.question.answers.map(
-//                     (_, i) => {
-//                         return <div className='Order' style={{ backgroundImage: `url(${props.question.background})` }}>
-//                             <FormControl style={{width: '50ch'}}
-//                                 key={i}
-//                                 disabled={props.show === undefined || props.show === false ? false : props.show}>
-//                                 <InputLabel>{i + 1}</InputLabel>
-//                                 <Select id={i.toString()} onChange={(e) =>
-//                                     handleChange(e.target.value as number, i)}
-//                                     value={props.answer && props.answer.order[i]}
-//                                     label="..">
-//                                     {props.question.answers.map(
-//                                         (answer, i) => {
-//                                             return <MenuItem value={i} key={i}>{answer.text}</MenuItem>
-//                                         }
-//                                     )}
-//                                 </Select>
-//                             </FormControl>
-//                         </div>
-//                     }
-//                 )
-//             }
-//             <Button onClick={() =>
-//                 props.callbackFinish && props.callbackFinish({
-//                     type: 'order',
-//                     isCorrect: JSON.stringify(question) == JSON.stringify(props.question.order),
-//                     order: question as number[]
-//                 })} disabled={question.includes(-1)}
-//             >Подтверждаю</Button>
-//         </div>
-//     </>
-// }
+class Item {
+    text: string
+    key: number
+    constructor(text: string, key: number) {
+        this.text = text
+        this.key = key
+    }
+}
+
+
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: 16,
+    maxHeight: "64px",
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "#1976d2",
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: 8,
+    height: "257px"
+});
+
+
 
 export function Order(props: OrderProps) {
-    const [items, setItems] = useState<string[]>([])
-    props.question.answers.map((answer) => { setItems((old) => [...old, answer.text]) })
-    console.log(items)
+    const [dragElems, setDragElems] = useState<Item[]>([])
+    useEffect(() => {
+        const items: Item[] = props.show && props.answer ?
+            props.answer.order.map((orderIndex) => new Item(props.question.answers[orderIndex].text, orderIndex)) :
+            props.question.answers.map((answer, i) => new Item(answer.text, i))
+        setDragElems(items)
+    }, [setDragElems])
 
-    const reorder = (list: string[], startIndex: number, endIndex: number) => {
+    const reorder = (list: Item[], startIndex: number, endIndex: number) => {
         const result = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
         result.splice(endIndex, 0, removed);
@@ -74,37 +63,66 @@ export function Order(props: OrderProps) {
             return;
         }
         const itemsNewOrder = reorder(
-            items,
+            dragElems,
             result.source.index,
             result.destination.index
         );
-        setItems(itemsNewOrder)
+        setDragElems(itemsNewOrder)
     }
 
-    return (<>
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="droppable">
-                {(provider) => (
-                    <div
-                        {...provider.droppableProps}
-                        ref={provider.innerRef}
-                    >
-                        {items.map((elem, i) => {
-                            return <Draggable key={elem} draggableId={elem} index={i}>
-                                {(provider) => (
-                                    <div
-                                        ref={provider.innerRef}
-                                        {...provider.draggableProps}
-                                        {...provider.dragHandleProps}
-                                    >
-                                        {<Button onClick={() => console.log(elem)}>{elem}</Button>}
-                                    </div>
+    function arraysEqual(a1: Array<any>, a2: Array<any>) {
+        return JSON.stringify(a1) == JSON.stringify(a2)
+    }
+
+    return <>
+        <div className='OrderBlock'>
+            <p className='text'>{props.question.text}</p>
+            {<img src={props.question.background} />}
+            <DragDropContext onDragEnd={onDragEnd}>
+                <div style={{ height: "320px", width: "90vw" }}>
+                    <Droppable droppableId='ksfsdkaj'>
+                        {(provider, snapshot) => (
+                            <List
+                                {...provider.droppableProps}
+                                ref={provider.innerRef}
+                                style={getListStyle(snapshot.isDraggingOver)}
+                            >
+                                {dragElems.map((e, i) =>
+                                    <Draggable isDragDisabled={props.show} key={e.key} draggableId={e.key.toString()} index={i}>
+                                        {
+                                            (provider, snapshot) => (
+                                                <ListItem
+                                                    key={e.key}
+                                                    ref={provider.innerRef}
+                                                    {...provider.draggableProps}
+                                                    {...provider.dragHandleProps}
+                                                    style={getItemStyle(
+                                                        snapshot.isDragging,
+                                                        provider.draggableProps.style)}
+                                                >
+                                                    <ListItemIcon>
+                                                        <DragHandle />
+                                                    </ListItemIcon>
+                                                    <ListItemText primary={e.text} />
+                                                </ListItem>
+                                            )
+                                        }
+                                    </Draggable>
                                 )}
-                            </Draggable>
-                        })}
-                    </div>
-                )}
-            </Droppable>
-        </DragDropContext></>
-    )
+                            </List>
+                        )}
+                    </Droppable>
+                </div>
+            </DragDropContext>
+
+            <Button disabled={props.show} onClick={() => {
+                const order = dragElems.map((e) => e.key)
+                const answer: AnswerOrder = { isCorrect: arraysEqual(order, props.question.order), order: order, type: 'order' }
+                props.callbackFinish && props.callbackFinish(answer)
+            }}
+            >
+                Подтверждаю
+            </Button>
+        </div>
+    </>
 }
